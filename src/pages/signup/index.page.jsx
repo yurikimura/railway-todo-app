a0fdaf8aa1150ba08672639a0e86235619fb850b
yurikimura <yuri.76135@gmail.com>
2025-08-11 19:05:37 +0900

    スタイリング

--- src/pages/signup/index.page.jsx
+++ src/pages/signup/index.page.jsx
@@ -43,56 +43,56 @@ const SignUp = () => {
   return (
     <div className="signup">
       <header className="signup__header">
-        <h1 className="signup__app_title">Todoアプリ</h1>
+          <h1 className="signup__app_title">Todoアプリ</h1>
       </header>
       <div className="signup__container">
-      <h2 className="signup__title">新規登録</h2>
-      {errorMessage && <div className="signup__error">{errorMessage}</div>}
-      <form className="signup__form" onSubmit={onSubmit}>
-        <fieldset className="signup__form_field">
-          <label htmlFor={`${id}-email`} className="signup__form_label">
-            メールアドレス
-          </label>
-          <Input
-            id={`${id}-email`}
-            type="email"
-            autoComplete="email"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-        </fieldset>
-        <fieldset className="signup__form_field">
-          <label htmlFor={`${id}-name`} className="signup__form_label">
-            名前
-          </label>
-          <Input
-            id={`${id}-name`}
-            type="text"
-            autoComplete="name"
-            value={name}
-            onChange={event => setName(event.target.value)}
-          />
-        </fieldset>
-        <fieldset className="signup__form_field">
-          <label htmlFor={`${id}-password`} className="signup__form_label">
-            パスワード
-        </label>
-          <Input
-            id={`${id}-password`}
-            type="password"
-            autoComplete="new-password"
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
-        </fieldset>
-        <div className="signup__form_actions">
-          <button
-            type="submit"
-            className="signup__primary_button"
-            disabled={isSubmitting}
-          >
-            作成
-          </button>
+        <h2 className="signup__title">新規登録</h2>
+        {errorMessage && <div className="signup__error">{errorMessage}</div>}
+        <form className="signup__form" onSubmit={onSubmit}>
+          <fieldset className="signup__form_field">
+            <label htmlFor={`${id}-email`} className="signup__form_label">
+              メールアドレス
+            </label>
+            <Input
+              id={`${id}-email`}
+              type="email"
+              autoComplete="email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+            />
+          </fieldset>
+          <fieldset className="signup__form_field">
+            <label htmlFor={`${id}-name`} className="signup__form_label">
+              名前
+            </label>
+            <Input
+              id={`${id}-name`}
+              type="text"
+              autoComplete="name"
+              value={name}
+              onChange={event => setName(event.target.value)}
+            />
+          </fieldset>
+          <fieldset className="signup__form_field">
+            <label htmlFor={`${id}-password`} className="signup__form_label">
+              パスワード
+            </label>
+            <Input
+              id={`${id}-password`}
+              type="password"
+              autoComplete="new-password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+            />
+          </fieldset>
+          <div className="signup__form_actions">
+            <button
+              type="submit"
+              className="signup__primary_button"
+              disabled={isSubmitting}
+            >
+              作成
+            </button>
           </div>
         </form>
       </div>
