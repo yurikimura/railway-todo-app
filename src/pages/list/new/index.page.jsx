5606d2b004577218243c30a586b32b60a1b0615a
yurikimura <yuri.76135@gmail.com>
2025-08-13 09:32:22 +0900

    リスト新規作成ページ作成

--- src/pages/list/new/index.page.jsx
+++ src/pages/list/new/index.page.jsx
@@ -1,21 +1,22 @@
 import React, { useCallback, useState } from 'react'
-import { Link, useHistory } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { BackButton } from '~/components/BackButton'
+import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import Button from '~/components/Button'
 import Input from '~/components/Input'
-import Textarea from '~/components/Textarea'
 import './index.css'
+import '~/pages/index.css'
 import { createList, setCurrentList } from '~/store/list/index'
 import { useId } from '~/hooks/useId'
+import { useLogout } from '~/hooks/useLogout'
 
 const NewList = () => {
   const id = useId()
   const history = useHistory()
   const dispatch = useDispatch()
+  const userName = useSelector(state => state.auth.user?.name)
+  const { logout } = useLogout()
 
   const [title, setTitle] = useState('')
-  const [detail, setDetail] = useState('')
 
   const [errorMessage, setErrorMessage] = useState('')
   const [isSubmitting, setIsSubmitting] = useState(false)
@@ -26,7 +27,7 @@ const NewList = () => {
 
       setIsSubmitting(true)
 
-      void dispatch(createList({ title, detail }))
+      void dispatch(createList({ title, detail: '' }))
         .unwrap()
         .then(listId => {
           dispatch(setCurrentList(listId))
@@ -39,50 +40,45 @@ const NewList = () => {
           setIsSubmitting(false)
         })
     },
-    [title, detail, dispatch, history]
+    [title, dispatch, history]
   )
 
   return (
-    <main className="new_list">
-      <BackButton />
-      <h2 className="new_list__title">New List</h2>
-      <p className="new_list__error">{errorMessage}</p>
-      <form className="new_list__form" onSubmit={onSubmit}>
-        <fieldset className="new_list__form_field">
-          <label htmlFor={`${id}-title`} className="new_list__form_label">
-            Name
-          </label>
-          <Input
-            id={`${id}-title`}
-            placeholder="買い物"
-            value={title}
-            onChange={event => setTitle(event.target.value)}
-          />
-        </fieldset>
-        <fieldset className="new_list__form_field">
-          <label htmlFor={`${id}-detail`} className="new_list__form_label">
-            Detail
-          </label>
-          <Textarea
-            id={`${id}-detail`}
-            placeholder="リストの詳細を入力してください"
-            value={detail}
-            onChange={event => setDetail(event.target.value)}
-            rows={3}
-            autoResize={true}
-          />
-        </fieldset>
-        <div className="new_list__form_actions">
-          <Link to="/" data-variant="secondary" className="app_button">
-            Cancel
-          </Link>
-          <div className="new_list__form_actions_spacer"></div>
-          <Button type="submit" disabled={isSubmitting}>
-            Create
-          </Button>
+    <div className="home">
+      <header className="home__header">
+        <h1 className="home__title">Todoアプリ</h1>
+        <div className="home__user">
+          <span className="home__user_name">{userName}</span>
+          <button onClick={logout} className="home__logout_button">
+            サインアウト
+          </button>
         </div>
-      </form>
-    </main>
+      </header>
+      <div className="signin__container">
+        <main className="new_list">
+          <h2 className="new_list__title">リスト新規作成</h2>
+          <p className="new_list__error">{errorMessage}</p>
+          <form className="new_list__form" onSubmit={onSubmit}>
+            <fieldset className="new_list__form_field">
+              <label htmlFor={`${id}-title`} className="new_list__form_label">
+                タイトル
+              </label>
+              <Input
+                id={`${id}-title`}
+                placeholder=""
+                value={title}
+                onChange={event => setTitle(event.target.value)}
+              />
+            </fieldset>
+            <div className="new_list__form_actions">
+              <Button type="submit" size="small" disabled={isSubmitting}>
+                作成
+              </Button>
+            </div>
+          </form>
+        </main>
+      </div>
+    </div>
   )
 }
 
